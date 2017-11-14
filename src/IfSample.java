b9c95ca5384f105058f5627c768a8a6d497ff24c
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-14 17:56:34 +0100

    Added test IfSample class

--- src/IfSample.java
+++ src/IfSample.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
+/*
+	оператор if.
+ */
 public class IfSample {
+	public static void main(String[] args) {
+		int x, y;
+		
+		x = 10;
+		y = 20;
+		
+		if (x < y) {
+			System.out.println("y меньше y ");
+		}
+		x*=2;
+		if (x == y) {
+			System.out.println("Теперь x = y ");
+		}
+		x += 5;
+		if (x>y) {
+			System.out.println("Сейчас x > y ");
+		}
+		
+	}
 }
