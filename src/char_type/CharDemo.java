842bd45f03a2ae371c56d9e4b9fe7486bd97418e
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-15 14:22:23 +0100

    Added CharDemo class

---
+++ src/char_type/CharDemo.java
@@ -0,0 +1,15 @@
+package char_type;
+
+/**
+ * Char Type - 16 bit ot 0 do 65536
+ *
+ */
+public class CharDemo {
+	public static void main(String[] args) {
+		char ch1, ch2;
+		ch1 = 88;   //код символа X
+		ch2 = 'Y';
+		System.out.println("ch1 = " + ch1);
+		System.out.println("ch2 = " + ch2);
+	}
+}
