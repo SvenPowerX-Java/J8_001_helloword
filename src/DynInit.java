2a63cc6154fbe022a480bb049225ea2b32233c3b
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-15 16:26:13 +0100

    Added DynInit class (Вычесление гипатенузы треугольника)

---
+++ src/DynInit.java
@@ -0,0 +1,15 @@
+/**
+ * Динамическая инициализация.
+ */
+
+
+public class DynInit {
+	public static void main(String[] args) {
+		double a = 3.0, b = 4.0;
+		
+		System.out.println("Катет a = " + a);
+		System.out.println("Катет b = " + b);
+		double c = Math.sqrt(a * a + b * b);
+		System.out.println("Гипатинуза c = " + c);
+	}
+}
