a68548296bdbf7d271469e202dd0d379c61d9cb0
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-15 13:17:37 +0100

    Added LongTypeTest class(программа растояние, проходимое светом)

---
+++ src/num/LongTypeTest.java
@@ -0,0 +1,35 @@
+package num;
+
+/**
+ * Long Type - 64 bit.
+ * Программа вычисляет количество миль,
+ * проходимых лучом света за указанное число дней.
+ */
+
+//  Вычислить растояние, проходимое свеом,
+//используя переменные типа long
+public class LongTypeTest {
+	
+	
+	public static void main(String[] args) {
+		int lightspeed;
+		long days;
+		long seconds;
+		long distance;
+		
+		// приблизительная скорость света, миль в секунду
+		lightspeed = 186000;
+		
+		//количество дней
+		days = 1000;
+		
+		// преобразовать в секунды
+		seconds  = days * 24 * 60 * 60;
+		
+		distance = lightspeed * seconds; //Вычислить растояние
+		
+		System.out.print("За " + days);
+		System.out.print(" дней свет пройдёт около ");
+		System.out.println(distance + " миль.");
+	}
+}
