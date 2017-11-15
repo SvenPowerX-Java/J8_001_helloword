152d31e4da539e633503f6acdfb1819f33964b6a
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-15 14:52:00 +0100

    Changed IntTypeTest class

--- src/num/IntTypeTest.java
+++ src/num/IntTypeTest.java
@@ -9,7 +9,7 @@ public class IntTypeTest {
 	public static void main(String[] args) {
 		int i = 0;
 		//int count1 = 2147483648 + 2147483647;
-		long count = 2147483;
+		long count = 2147483648L +5;
 		
 		for (long j = 0; j < count; i++, j++) {
 			if (i > 2147483645 || i < -2147483646) {
