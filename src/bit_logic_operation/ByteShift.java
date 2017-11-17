1c2e9c46f43ea6216cc002bced2a10638363dade
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-17 15:27:32 +0100

    Bytehift class (Сдвиг влево значение типа byte)

---
+++ src/bit_logic_operation/ByteShift.java
@@ -0,0 +1,15 @@
+package bit_logic_operation;
+
+//Сдвиг влево значения типа byte
+class ByteShift {
+	public static void main(String[] args) {
+		byte a = 64, b;
+		int i;
+		
+		i = a << 2;
+		b = (byte) (a << 2);
+		System.out.println("Первоначальное значение a :" + a);
+		System.out.println("i and b: " + i + " " + b);
+	}
+	
+}
