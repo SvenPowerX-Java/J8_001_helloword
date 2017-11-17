77bd9387e93575363e8da83838dfe10e2a62fa37
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-17 16:31:08 +0100

    Added OpBitEquals class Составные побитовые операторы присваивания

---
+++ src/bit_logic_operation/OpBitEquals.java
@@ -0,0 +1,20 @@
+package bit_logic_operation;
+
+/**
+ * Составные побитовые операторы присваивания
+ */
+public class OpBitEquals {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		
+		a |= 4;
+		b >>= 1;
+		c <<= 1;
+		a ^= c;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+	}
+}
