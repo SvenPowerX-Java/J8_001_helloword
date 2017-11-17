6c08156705843c27ad40966f1d1696b1479a3676
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-17 11:50:56 +0100

    Добавленны классы арифметичиских и логических операций

---
+++ src/basic_math/Modulus.java
@@ -0,0 +1,14 @@
+package basic_math;
+
+/**
+ * Деление по модулю
+ */
+public class Modulus {
+	public static void main(String[] args) {
+		int x = 42;
+		double y = 42.25;
+		
+		System.out.println("x mod 10 = " + x % 10);
+		System.out.println("y mod = " + y % 10);
+	}
+}
---
+++ src/bit_logic_operation/ToBinaryString.java
@@ -0,0 +1,16 @@
+package bit_logic_operation;
+
+/**
+ * Вывод числа на консоль в бинарном виде
+ */
+public class ToBinaryString {
+	public static void main(String[] args) {
+		int x = 3;
+		System.out.println("Integer.toBinaryString(1) = " + Integer.toBinaryString(0));
+		System.out.println("Integer.toBinaryString(1) = " + Integer.toBinaryString(1));
+		System.out.println("Integer.toBinaryString(2) = " + Integer.toBinaryString(2));
+		System.out.println("Integer.toBinaryString(x) = " + Integer.toBinaryString(x));
+		
+	}
+	
+}
