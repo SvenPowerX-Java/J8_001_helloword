1b709f2b4db47fb693a0f64feeddc14f6bd1f89f
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-17 07:45:27 +0100

    Added AlternatSintaxInitArray  class.(Альтернативный синтаксис объявления массивов.)

---
+++ src/arrays/AlternatSintaxInitArray.java
@@ -0,0 +1,18 @@
+package arrays;
+
+/**
+ * Альтернативный синтаксис объявления массивов
+ */
+public class AlternatSintaxInitArray {
+	public static void main(String[] args) {
+		int arr1[] = new int[3];
+		int[] arr2 = new int[3];
+		
+		char twod1[][] = new char[3][4];
+		char[][] twod2 = new char[3][4];
+		
+		//создаём три массива
+		int[] nums, nums2, nums3;
+		int nums_arr[], nums_arr2[],nums_arr3[];
+	}
+}
