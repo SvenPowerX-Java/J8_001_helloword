06d29710758dd54c2540df34e2de9dcb2a63e58c
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-17 12:17:21 +0100

    Changed  value x to ToBinaryString class

--- src/bit_logic_operation/ToBinaryString.java
+++ src/bit_logic_operation/ToBinaryString.java
@@ -5,7 +5,7 @@ package bit_logic_operation;
  */
 public class ToBinaryString {
 	public static void main(String[] args) {
-		int x = 3;
+		int x = -2;
 		System.out.println("Integer.toBinaryString(1) = " + Integer.toBinaryString(0));
 		System.out.println("Integer.toBinaryString(1) = " + Integer.toBinaryString(1));
 		System.out.println("Integer.toBinaryString(2) = " + Integer.toBinaryString(2));
