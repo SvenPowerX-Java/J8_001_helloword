f11a9d357fcf74addcd97334b164d6fc723370b6
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-17 07:34:08 +0100

    Added TreeDMatrix class.(Демонстрация применения трёхмерного массива форматированным выводом.printf)

---
+++ src/arrays/TreeDMatrix.java
@@ -0,0 +1,26 @@
+package arrays;
+
+/**
+ * Демонстрация применения трёхмерного массива
+ */
+public class TreeDMatrix {
+	public static void main(String[] args) {
+		int threeD[][][] = new int[3][4][5];
+		int i, j, k;
+		
+		for (i=0; i<3;i++)
+			for (j=0;j<4;j++)
+				for (k=0;k<5;k++)
+					threeD[i][j][k] = i*j*k;
+		
+		for (i=0;i<3;i++) {
+			for (j=0;j<4;j++) {
+				for (k=0;k<5;k++) {
+					System.out.printf("%5d",threeD[i][j][k]);
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+	}
+}
