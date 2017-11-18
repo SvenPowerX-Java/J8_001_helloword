889e0b03bf5e2ed2e37e47600748457bda687f12
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-18 13:07:29 +0100

    Added Return class

---
+++ src/block_and_break_and_continue/Return.java
@@ -0,0 +1,10 @@
+package block_and_break_and_continue;
+
+public class Return {
+	public static void main(String[] args) {
+		boolean t = true;
+		System.out.println("До возврата.");
+		if (t)return;
+		System.out.println("Этот оператор выполнятся небудет");
+	}
+}
