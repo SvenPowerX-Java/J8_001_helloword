0586289c0eee6ac2820a3bd38a8bd01e64824bc2
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-18 12:32:33 +0100

    Added Break class (break в качестве оператора goto).

---
+++ src/block_and_break/Beak.java
@@ -0,0 +1,21 @@
+package block_and_break;
+
+/**
+ * Применение оператора break в качестве оператора goto
+ */
+public class Beak {
+	public static void main(String[] args) {
+		boolean t = true;
+		first: {
+			second:{
+				third:{
+					System.out.println("Предшествует оператору break.");
+					if (t) break second;
+					System.out.println("Этот оператор небудет выполнятся");
+				}
+				System.out.println("Этот оператор небудет выполнятся");
+			}
+			System.out.println("Этот оператор следует за болоком second");
+		}
+	}
+}
