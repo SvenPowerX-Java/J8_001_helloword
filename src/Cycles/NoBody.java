25b0ad72aeee7a813333cc65781bfbcded660dea
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-18 07:19:14 +0100

    Added NoBody class(Цикл while без тела).

---
+++ src/Cycles/NoBody.java
@@ -0,0 +1,18 @@
+package Cycles;
+
+/**
+ * Целева часть цикла может быть пустой
+ */
+public class NoBody {
+	public static void main(String[] args) {
+		int i, j;
+		
+		i = 100;
+		j = 200;
+		
+		//расчитать среднее значение переменных i и j
+		while (++i < --j);// у этого цикла отсутствует тело
+		
+		System.out.println("Среднее значение равно " + i);
+	}
+}
