973b82f6596e411caa533e971ffd554df2e0bf8c
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-18 09:39:54 +0100

    Changed ForTick class

--- src/Cycles/ForTick.java
+++ src/Cycles/ForTick.java
@@ -6,8 +6,16 @@ package Cycles;
 public class ForTick {
 	public static void main(String[] args) {
 		int n;
+		System.out.println("Цикл с n");
 		for (n = 10; n > 0; n--) {
-			System.out.println("Такт = " + n);
+			if (n>1)
+			System.out.print("Такт:" + n+", ");
+			else System.out.println("Такт:" + n+". ");
+		}
+		System.out.println("Цикл с i");
+		for (int i = 10; i > 0; i--) {
+			if (i>1)System.out.print("Такт:" + i + ", ");
+			else System.out.println("Такт:" + i+".");
 		}
 	}
 }
