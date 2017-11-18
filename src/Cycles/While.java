88572b0698ad2124fcf159ccdbbcd134c9b7fdca
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-18 07:07:56 +0100

    Added While class(Оператор цикла while).

---
+++ src/Cycles/While.java
@@ -0,0 +1,14 @@
+package Cycles;
+
+/**
+ * Оператор цикла while
+ */
+public class While {
+	public static void main(String[] args) {
+		int n = 10;
+		while (n > 0) {
+			System.out.println("такт " + n);
+			n--;
+		}
+	}
+}
