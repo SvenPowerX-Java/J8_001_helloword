3ba554f01c4a1fac8b232fede8b287a9bd3cc4fe
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-18 11:03:27 +0100

    Added ForEach3 and Search class

---
+++ src/Cycles/ForEach3.java
@@ -0,0 +1,27 @@
+package Cycles;
+
+/**
+ * foreach для двумерного массива
+ */
+public class ForEach3 {
+	public static void main(String[] args) {
+		int sum = 0;
+		int nums[][] = new int[3][5];
+		
+		//присвоить знчение элементам массива nums
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 5; j++) {
+				nums[i][j] = (i + 1) * (j + 1);
+			}
+		}
+		for (int x[] :
+				nums) {
+			for (int y :
+					x) {
+				System.out.println("Значение равно: " + y);
+				sum += y;
+			}
+		}
+		System.out.println("sum = " + sum);
+	}
+}
