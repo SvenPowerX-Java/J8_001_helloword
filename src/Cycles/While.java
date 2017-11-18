94bd222db4fd9d4efe2021cdf8801e1aab85ff92
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-18 07:13:34 +0100

    Changed While class

--- src/Cycles/While.java
+++ src/Cycles/While.java
@@ -6,6 +6,18 @@ package Cycles;
 public class While {
 	public static void main(String[] args) {
 		int n = 10;
+		int a = 10, b = 20;
+		
+		while (a>b)
+			System.out.println("Эта строка 1 выводится не будет");
+		
+	/*
+	// Вечный цикл
+	while (!(a>b))
+			System.out.println("Эта строка 2 выводится будет");
+		while (a<b)
+			System.out.println("Эта строка 3 выводится будет");*/
+		
 		while (n > 0) {
 			System.out.println("такт " + n);
 			n--;
